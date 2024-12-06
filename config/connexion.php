c69a22650445144ca776675edfc924dcfcb2e066
Eric Se <ericse606@gmail.com>
2024-12-06 17:05:26 +0100

    ajout des fichiers web

---
+++ config/connexion.php
@@ -0,0 +1,40 @@
+
+    <?php
+
+    class Connexion {
+        // Les attributs static caractristiques de la connexion
+        static private $hostname = 'localhost';
+        static private $database = 'saes3-ese';
+        static private $login = 'saes3-ese';
+        static private $password = 'kU4Ny1JywAGjEybF';
+
+
+        static private $tabUTF8 = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+
+
+        // L'atribut static qui matérialisera la connexion
+        static private $pdo;
+
+
+        // Le getter public de cet attribut
+        static public function pdo() {return self::$pdo;}
+
+        // la fonction static de connexion qui initialise $pdo et lance la tentative de connecxion
+
+        static public function connect() {
+            $h= self::$hostname; $d=self::$database; $l = self::$login;
+            $p = self::$password; $t=self::$tabUTF8;
+
+            try {
+                self::$pdo = new PDO("mysql:host=$h;dbname=$d",$l,$p,$t);
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            } catch (PDOException $e) {
+                echo "erreur de connexion : ".$e->getMessage()."<br>";
+            }
+        }
+    }
+
+
+
+    ?>
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+    require_once("config/connexion.php");
+    Connexion::connect();
+?>
