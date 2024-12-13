7baee2927a89f317a1599f721143ebcb1a9a4f39
Eric <eric.se@universite-paris-saclay.fr>
2024-12-13 16:26:45 +0100

    ajout

--- README.md
+++
@@ -1,1 +0,0 @@
-# projetSAES3
--- controllers/controleurconnexion.php
+++ controllers/controleurconnexion.php
@@ -1,6 +1,24 @@
 <?php
-require_once("modele/utilisateur.php");
+require_once("../config/connexion.php");
+require_once("../modele/utilisateur.php");
 
+Connexion::connect();
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Rechercher l'utilisateur dans la base de données
+    $stmt = $pdo->prepare("SELECT * FROM utilisateur WHERE user_email = :email");
+    $stmt->execute(['email' => $email]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Vérification du mot de passe
+    if ($user && password_verify($password, $user['password'])) {
+        echo "Connexion réussie. Bienvenue, " . htmlspecialchars($user['email']) . "!";
+    } else {
+        echo "Adresse e-mail ou mot de passe incorrect.";
+    }
+}
 
 ?>
