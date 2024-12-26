a9f4269a69dca9d585b8c52dbc292e2bb712274a
EricPauLitique <ericse606@gmail.com>
2024-12-26 02:58:00 +0100

    Add styles for group titles and links; refactor HTML structure for better semantics

--- vue/accueil.php
+++ vue/accueil.php
@@ -74,7 +74,7 @@ $id = htmlspecialchars($_SESSION['id']);
                                             <img src="' . $listGrp->get('grp_img') . '" alt="Logo ' . $listGrp->get('grp_nom') . '" class="image-small" />
                                         </a>
                                     </div>
-                                       <b><h2 style="color: ' . $couleur . ';"><a href="groupe.php?id=' . $listGrp->get('grp_id') . '">' . strtoupper($listGrp->get('grp_nom')) . '</a></h2></b>
+                                    <h2 class="group-title" style="color: ' . $couleur . ';"><a href="groupe.php?id=' . $listGrp->get('grp_id') . '" class="group-link">' . strtoupper($listGrp->get('grp_nom')) . '</a></h2>
                                 </div>
                                 <div class="boutons-container">
                                     <form method="POST" action="../controllers/controleurmodifGroupe.php" style="display:inline;">
@@ -100,14 +100,14 @@ $id = htmlspecialchars($_SESSION['id']);
                                             <img src="' . $listGrp->get('grp_img') . '" alt="Logo ' . $listGrp->get('grp_nom') . '" class="image-small" />
                                         </a>
                                     </div>
-                                        <b><h2 style="color: ' . $couleur . ';"><a href="groupe.php?id=' . $listGrp->get('grp_id') . '">' . strtoupper($listGrp->get('grp_nom')) . '</a></h2></b>
-                                    </div>
-                                    <div class="boutons-container">
-                                        <form method="POST" action="../controllers/controleurQuitterGroupe.php" style="display:inline;">
-                                            <input type="hidden" name="user_id" value="' . $id . '">
-                                            <input type="hidden" name="grp_id" value="' . $listGrp->get('grp_id') . '">
-                                            <button type="submit" class="btn-delete btn-quitter" style="width: 93.81px; height: 35px;" onclick="return confirm(\'Êtes-vous sûr de vouloir quitter ce groupe ?\');">Quitter</button>                                        </form>
-                                    </div>
+                                    <h2 class="group-title" style="color: ' . $couleur . ';"><a href="groupe.php?id=' . $listGrp->get('grp_id') . '" class="group-link">' . strtoupper($listGrp->get('grp_nom')) . '</a></h2>
+                                </div>
+                                <div class="boutons-container">
+                                    <form method="POST" action="../controllers/controleurQuitterGroupe.php" style="display:inline;">
+                                        <input type="hidden" name="user_id" value="' . $id . '">
+                                        <input type="hidden" name="grp_id" value="' . $listGrp->get('grp_id') . '">
+                                        <button type="submit" class="btn-delete btn-quitter" style="width: 93.81px; height: 35px;" onclick="return confirm(\'Êtes-vous sûr de vouloir quitter ce groupe ?\');">Quitter</button>                                        </form>
+                                </div>
                               </li>';
                     }
                 }
